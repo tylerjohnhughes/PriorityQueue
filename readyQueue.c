1431206d26b945ff0f2debc46a92eaf11e72008f
Tyler Hughes <tylerjohnhughes@me.com>
2015-10-07 21:40:59 -0700

    Initial commit.

---
+++ readyQueue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "readyQueue.h"
+
+
+PCB *PriorityQueue_dequeue(PriorityQueue *queue) {
+    return NULL;
+}
+
+void PriorityQueue_enqueue(PriorityQueue *queue, PCB *pcb) {
+    
+}
+
+void PriorityQueue_traverse(PriorityQueue *queue) {
+    
+}
---
+++ readyQueue.h
@@ -0,0 +1,44 @@
+#ifndef READY_QUEUE_H
+#define READY_QUEUE_H
+
+#define PRIORITY_CLASSES 64
+
+
+typedef enum state_types {
+    NEW,
+    READY,
+    RUNNING,
+    WAITING,
+    HALTED
+} StateType;
+
+
+typedef struct pcb {
+    int processID;
+    int priority;
+    StateType state;
+} PCB;
+
+typedef struct pcb_node {
+    PCB *pcb;
+    struct pcb_node *next;
+} PCBNode;
+
+typedef struct fifo_queue {
+    PCBNode *head;
+    PCBNode *tail;
+} FIFOQueue;
+
+typedef struct priority_queue {
+    FIFOQueue queues[PRIORITY_CLASSES];
+} PriorityQueue;
+
+
+PCB *PriorityQueue_dequeue(PriorityQueue *queue);
+
+void PriorityQueue_enqueue(PriorityQueue *queue, PCB *pcb);
+
+void PriorityQueue_traverse(PriorityQueue *queue);
+
+
+#endif
